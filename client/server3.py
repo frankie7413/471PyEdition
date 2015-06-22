07b467883aa57a9637443f0cfa003542033d749a
Francisco Rivas <frankie7413@gmail.com>
2015-06-22 00:31:48 -0700

    Ready to make the car pages

--- client/server3.py
+++ client/server3.py
@@ -5,6 +5,7 @@ PORT = 8008
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
+#change ip and port since i am testing on server 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
