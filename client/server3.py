95b338a1af40f994a7dc75cec2edd3dd942f892b
Frankie <Frankie@Frankies-MacBook-Pro.local>
2015-06-23 12:21:20 -0700

    updated html

--- client/server3.py
+++ client/server3.py
@@ -1,13 +1,13 @@
 import SimpleHTTPServer
 import SocketServer
 #http://stackoverflow.com/questions/19571282/using-forever-js-with-python
-#default port for dev is 8008
+#default port for dev is 8008 prod is 8080
 PORT = 8080
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 #change ip and port since i am testing on server
-#quotations empty for dev 
+#quotations empty for dev 45.55.220.106
 httpd = SocketServer.TCPServer(("45.55.220.106", PORT), Handler)
 
 print "serving at port", PORT
